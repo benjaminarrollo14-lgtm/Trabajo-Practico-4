ca3dd07bc9ca40212f4df6caf2bb779c826cb32d
Benja <benjaminarrollo14@gmail.com>
2025-09-02 17:14:00 -0300

    Update trabajo practico.py

---
+++ trabajo practico.py
@@ -0,0 +1,16 @@
+
+
+#actividad 10
+# cadena = input("Ingrese una cadena de texto: ")
+# print("Ingrese si quiere Mayusculas o Minusculas")
+# opcion = input("M para Mayusculas, m para Minusculas: ")
+# if opcion == "M":
+#     print(cadena.upper())  
+# elif opcion == "m":
+#     print(cadena.lower())
+
+#actividad 11
+
+cadena = "hipopótamo"
+extraer = cadena[3:5]
+print(extraer)

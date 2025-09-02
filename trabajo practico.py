c5704e6f255e090bb9772817d85b3cb59dfc7748
Benja <benjaminarrollo14@gmail.com>
2025-09-02 17:16:05 -0300

    Puntos 10 y 14 terminados
    agregar ultimos ejercicos los demas

--- trabajo practico.py
+++ trabajo practico.py
@@ -10,7 +10,27 @@
 #     print(cadena.lower())
 
 #actividad 11
+# palabra = input("Ingrese una palabra: ")
+# palabra2 = input("Ingrese otra palabra: ")
+# if palabra == palabra2:
+#     print("Las palabras son iguales")
+# else:
+#     print("Las palabras son diferentes")
 
-cadena = "hipopótamo"
-extraer = cadena[3:5]
-print(extraer)
+#actividad 12
+# cadena = "hipopótamo"
+# extraer = cadena[3:5]
+# print(extraer)
+
+#actividad 13
+# cadena1 = input("Ingrese la primera cadena de texto: ")
+# cadena2 = input("Ingrese la segunda cadena de texto: ")
+# if cadena2 in cadena1:
+#     print("La segunda cadena se encuentra dentro de la primera.")
+# else:
+#     print("La segunda cadena no se encuentra dentro de la primera.")
+
+#actividad 14
+# En Python, no existe una distinción estricta entre variables de tipo valor y tipo referencia como en Java.
+# Todas las variables en Python son referencias a objetos. Sin embargo, los tipos inmutables (como int, float, str, tuple) se comportan de manera similar a los tipos valor,
+# mientras que los tipos mutables (como list, dict, set) se comportan como tipos referencia.

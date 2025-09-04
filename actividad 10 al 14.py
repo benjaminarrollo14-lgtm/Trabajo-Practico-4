73934637f75a83a7c194590a02b44770938da09f
Benja <benjaminarrollo14@gmail.com>
2025-09-04 19:25:49 -0300

    ejercicios 10 al 14

---
+++ actividad 10 al 14.py
@@ -0,0 +1,34 @@
+#actividad 10
+# cadena = input("Ingrese una cadena de texto: ")
+# print("Ingrese si quiere Mayusculas o Minusculas")
+# opcion = input("M para Mayusculas, m para Minusculas: ")
+# if opcion == "M":
+#     print(cadena.upper())  
+# elif opcion == "m":
+#     print(cadena.lower())
+
+#actividad 11
+# palabra = input("Ingrese una palabra: ")
+# palabra2 = input("Ingrese otra palabra: ")
+# if palabra == palabra2:
+#     print("Las palabras son iguales")
+# else:
+#     print("Las palabras son diferentes")
+
+#actividad 12
+# cadena = "hipopótamo"
+# extraer = cadena[3:5]
+# print(extraer)
+
+#actividad 13
+# cadena1 = input("Ingrese la primera cadena de texto: ")
+# cadena2 = input("Ingrese la segunda cadena de texto: ")
+# if cadena2 in cadena1:
+#     print("La segunda cadena se encuentra dentro de la primera.")
+# else:
+#     print("La segunda cadena no se encuentra dentro de la primera.")
+
+#actividad 14
+# En Python, no existe una distinción estricta entre variables de tipo valor y tipo referencia como en Java.
+# Todas las variables en Python son referencias a objetos. Sin embargo, los tipos inmutables (como int, float, str, tuple) se comportan de manera similar a los tipos valor,
+# mientras que los tipos mutables (como list, dict, set) se comportan como tipos referencia.
